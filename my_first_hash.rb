12a05bcbf86062467ba04ac99422f27e098c6e88
Yasmine Hajaji <yasminehajaji95@gmail.com>
2019-09-13 21:21:33 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -35,7 +35,7 @@ shipping_manifest << {"muskets" => 2}
 
 
   # add 4 gun powder to the shipping_manifest hash below
-
+shipping_manifest << {"gun powder" => 4}
 
 
   # return the shipping_manifest hash below
